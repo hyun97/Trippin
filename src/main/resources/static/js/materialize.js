d9e989c91de1795619db67995db462e14819ac39
hyun97 <nkh1602@naver.com>
2020-02-08 17:08:43 +0900

    Add Navbar

---
+++ src/main/resources/static/js/materialize.js
@@ -0,0 +1,14 @@
+// Side bar
+$(document).ready(function () {
+    $('.sidenav').sidenav();
+});
+
+// Modal
+$(document).ready(function () {
+    $('.modal').modal();
+});
+
+// Select
+$(document).ready(function () {
+    $('select').formSelect();
+});
