8a2074a0af33fc663f5eeab7599b36026aaa760c
hyun97 <nkh1602@naver.com>
2020-02-24 17:25:02 +0900

    test user feel

--- src/main/java/com/trippin/config/auth/CustomOAuth2UserService.java
+++ src/main/java/com/trippin/config/auth/CustomOAuth2UserService.java
@@ -55,7 +55,7 @@ public class CustomOAuth2UserService implements OAuth2UserService<OAuth2UserRequ
                     .name(attributes.getName())
                     .email(attributes.getEmail())
                     .picture(attributes.getPicture())
-                    .feel("상태메시지 없음")
+                    .feel("상태메시지")
                     .role(Role.USER)
                     .build();
 
